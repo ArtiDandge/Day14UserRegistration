66311da330ae004d9204265f27e0cfccf6b6fb1c
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-24 20:35:46 +0530

    First name validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
+read -p "enter first name " fname
 
+#fnamePattern="^[A-Z]{1}[a-z]{2,}$";
+pat="^[[:upper:]]{1}[a-z]{2,}$"
+if [[ $fname =~ $pat ]]
+then
+	echo "matched";
+else
+	echo "not matched";
+fi
