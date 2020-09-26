3044b1ea0629402cb954c35d84dabbb9527ac70c
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-26 16:07:30 +0530

    add mobile number with its validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -x
 
 #input first name and last name as fname and lname
-read -p "enter first name and last name" fname lname
+read -p "enter first name and last name " fname lname
 
 #input email address
 read -p "enter email address " email
 
 #inpur mobileNo
-read -p "enter mobile number formate: country code followed by space followed by 10digit mob. number" mobileNo 
+read -p "enter mobile number formate: country code followed by space followed by 10digit mob. number " mobileNo 
 
+#patterns for name, email and mobileNo matching
 namePattern="^[[:upper:]]{1}[a-z]{2,}$";
-emailPattern="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$";
-mobilePattern="^([9]{1}[1]{1})/s[6-9]{1}[0-9]{9}$";
+emailPattern="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
+mobPattern="^([+]{1}[9]{1}[1]{1})\s[6-9]{1}[0-9]{9}$"
 
 if [[ $fname =~ $namePattern ]] && [[ $lname =~ $namePattern ]]
 then
-	if [[ $email =~ $emailPattern ]] && [[ $mobileNo =~ $mobilePattern ]]
+	if [[ $email=~$emailPattern ]] && [[ $mobileNo=~$mobPattern ]] 
 	then
-		echo "matched";
+		echo "name ,email and mobile number validation successfull";
+	else
+		echo "email and mobile number validation not successfull";
 	fi
-
 else
-	echo "not matched";
+	echo "name not matched";
 fi
