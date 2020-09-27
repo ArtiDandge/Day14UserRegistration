f2348d1ff60ce0ec1fe55289d4ed8c9bb22c5c22
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-27 19:19:55 +0530

    modify pattern, add one special symbol

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,20 +6,35 @@ read -p "enter mobile number formate: country code followed by space followed by
 read -p "enter password " password
 
 #patterns for name, email, mobileNo and password validation
-namePattern="^[[:upper:]]{1}[a-z]{2,}$";
-emailPattern="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
-mobPattern="^([+]{1}[9]{1}[1]{1})\s[6-9]{1}[0-9]{9}$"
-password_validate="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+namePattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
+emailPattern="^([A-Za-z]+[A-Za-z0-9]*((\.|\-|\_)?[A-Za-z]+[A-Za-z0-9]*){0,})@(([A-Za-z]+[A-Za-z0-9]*)+((\.|\-|\_)?([A-Za-z]+[A-Za-z0-9]*)+){0,})+\.([A-Za-z]{2,})+$"
+mobPattern="^([+]{1}[9]{1}[1]{1})[[:space:]]|[6-9]{1}[0-9]{9}$"
+password_validate="(([[:upper:]]{1,}[0-9]{1,}[[:lower:]]{1,}[\!@#$%*&~?]{1,})?).{8,}$"
 
 if [[ $first_name =~ $namePattern ]] && [[ $last_name =~ $namePattern ]]
 then
-        if [[ $email=~$emailPattern ]] && [[ $mobileNo=~$mobPattern ]] && [[ $password=~$password_validate ]]
-        then
-                echo "name ,email,mobile number and password validation successfull";
-        else
-                echo "email or mobile number or password validation not successfull";
-        fi
+        echo "first name and last name matched";
 else
         echo "name not matched";
 fi
 
+if [[ $mobileNo =~ $mobPattern ]]
+then
+        echo "mobNo matched"
+else
+        echo "mobNo not mathed"
+fi
+
+if [[ $email =~ $emailPattern ]]
+then
+        echo "email matched"
+else
+        echo "email not matched"
+fi
+
+if  [[ $password =~ $password_validate ]]
+then
+        echo "password matched"
+else
+        echo "password not matched"
+fi
