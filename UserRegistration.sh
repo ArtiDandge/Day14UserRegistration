1ff3c40d50eb56926ff2f174e00bc8fe905f9d34
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-27 11:45:29 +0530

    add password and comment modification

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,29 +1,25 @@
 #!/bin/bash -x
 
-#input first name and last name as fname and lname
-read -p "enter first name and last name " fname lname
-#input email address
+read -p "enter first name and last name " first_name last_name
 read -p "enter email address " email
-#inpur mobileNo
-read -p "enter mobile number formate: country code followed by space followed by 10digit mob. number " mobileNo 
-#input password
+read -p "enter mobile number formate: country code followed by space followed by 10 digit mobile number " mobileNo
 read -p "enter password " password
 
-#patterns for name, email and mobileNo matching
+#patterns for name, email, mobileNo and password validation
 namePattern="^[[:upper:]]{1}[a-z]{2,}$";
 emailPattern="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
 mobPattern="^([+]{1}[9]{1}[1]{1})\s[6-9]{1}[0-9]{9}$"
-#pattern for password
-password_validate="^[A-za-z]{8,}$"
+password_validate="^[A-Za-z0-9]{8,}$/"
 
-if [[ $fname =~ $namePattern ]] && [[ $lname =~ $namePattern ]]
+if [[ $first_name =~ $namePattern ]] && [[ $last_name =~ $namePattern ]]
 then
-	if [[ $email=~$emailPattern ]] && [[ $mobileNo=~$mobPattern ]] && [[ $password=~$password_validate ]]
-	then
-		echo "name ,email,mobile number and password validation successfull";
-	else
-		echo "email or mobile number or password validation not successfull";
-	fi
+        if [[ $email=~$emailPattern ]] && [[ $mobileNo=~$mobPattern ]] && [[ $password=~$password_validate ]]
+        then
+                echo "name ,email,mobile number and password validation successfull";
+        else
+                echo "email or mobile number or password validation not successfull";
+        fi
 else
-	echo "name not matched";
+        echo "name not matched";
 fi
+
