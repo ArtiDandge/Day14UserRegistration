4516b7140aa1cb998187214d9505436c8b2958d2
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-27 12:28:04 +0530

    modify variables

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-read -p "enter first name " fname
+read -p "enter first name " first_name
 
 #fnamePattern="^[A-Z]{1}[a-z]{2,}$";
 pat="^[[:upper:]]{1}[a-z]{2,}$"
-if [[ $fname =~ $pat ]]
+if [[ $first_name =~ $pat ]]
 then
 	echo "matched";
 else
