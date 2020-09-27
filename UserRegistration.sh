16d383fa95084ac7b45f337bf2859f9c35c92fc2
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-27 19:16:30 +0530

    add one Upper case letter to password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,9 +6,9 @@ read -p "enter mobile number formate: country code followed by space followed by
 read -p "enter password " password
 
 #patterns for name, email, mobileNo and password validation
-namePattern="^[[:upper:]]{1}[a-z]{2,}$";
-emailPattern="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
-mobPattern="^([+]{1}[9]{1}[1]{1})\s[6-9]{1}[0-9]{9}$"
+namePattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
+emailPattern="^([A-Za-z]+[A-Za-z0-9]*((\.|\-|\_)?[A-Za-z]+[A-Za-z0-9]*){0,})@(([A-Za-z]+[A-Za-z0-9]*)+((\.|\-|\_)?([A-Za-z]+[A-Za-z0-9]*)+){0,})+\.([A-Za-z]{2,})+$"
+mobPattern="^([+]{1}[9]{1}[1]{1})[[:space:]]|[6-9]{1}[0-9]{9}$"
 password_validate="(([[:upper:]]{1,})?).{8,}$/"
 
 
