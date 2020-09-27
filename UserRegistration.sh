65eb534f03a9620aa13cc744db76cd6dfc42b73a
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-27 12:26:27 +0530

    modify valiables

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,18 +1,20 @@
 #!/bin/bash -x
 
-read -p "enter first name " fname lname
+read -p "enter first name and last name" first_name last_name
 read -p "enter email address " email
 namePattern="^[[:upper:]]{1}[a-z]{2,}$";
 emailPattern="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$";
 
 
-if [[ $fname =~ $namePattern ]] && [[ $lname =~ $namePattern ]]
+if [[ $first_name =~ $namePattern ]] && [[ $last_name =~ $namePattern ]]
 then
 	if [[ $email =~ $emailPattern ]]
 	then
-		echo "matched";
+		echo "name and email validated";
+	else
+		echo "email of validated"
 	fi
 
 else
-	echo "not matched";
+	echo "name not matched validation";
 fi
