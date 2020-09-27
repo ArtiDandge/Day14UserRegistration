449c03c9b955e81b738a5b9d901c1991132d83f8
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-27 19:07:15 +0530

    chech atleat one upper letter should ther in password

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -9,17 +9,34 @@ read -p "enter password " password
 namePattern="^[[:upper:]]{1}[a-z]{2,}$";
 emailPattern="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/"
 mobPattern="^([+]{1}[9]{1}[1]{1})\s[6-9]{1}[0-9]{9}$"
-password_validate="^(?=.*[A-Z])[A-Za-z0-9]{8,}$/"
+password_validate="(([[:upper:]]{1,})?).{8,}$/"
+
 
 if [[ $first_name =~ $namePattern ]] && [[ $last_name =~ $namePattern ]]
 then
-        if [[ $email=~$emailPattern ]] && [[ $mobileNo=~$mobPattern ]] && [[ $password=~$password_validate ]]
-        then
-                echo "name ,email,mobile number and password validation successfull";
-        else
-                echo "email or mobile number or password validation not successfull";
-        fi
+        echo "first name and last name matched";
 else
         echo "name not matched";
+fi
+
+if [[ $mobileNo =~ $mobPattern ]]
+then
+        echo "mobNo matched"
+else
+        echo "mobNo not mathed"
+fi
+
+if [[ $email =~ $emailPattern ]]
+then
+        echo "email matched"
+else
+        echo "email not matched"
+fi
+
+if  [[ $password =~ $password_validate ]]
+then
+        echo "password matched"
+else
+        echo "password not matched"
 fi
 
